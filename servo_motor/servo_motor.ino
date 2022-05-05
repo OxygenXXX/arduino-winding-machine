3d01f0efbe9e19ffa408e4b2ec95dde0526f15ff
OxygenXXX <oxyyygen.developer@gmail.com>
2022-05-05 07:17:37 +0300

    Servo base

--- servo_motor/servo_motor.ino
+++ servo_motor/servo_motor.ino
@@ -1,1 +1,13 @@
 #include <Servo.h>
+
+#define SERVO_PIN_S1 1 // SELECT 
+#define SERVO_PIN_S2 2 // SELECT
+
+Servo SERVO_CONTROL_S1;
+Servo SERVO_CONTROL_S2;
+
+void ServoMotorInit()
+{
+    SERVO_CONTROL_S1.attach(SERVO_PIN_S1);
+    SERVO_CONTROL_S1.attach(SERVO_PIN_S2);
+}
